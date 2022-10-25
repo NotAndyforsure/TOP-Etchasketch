dd7195d496e60551f2a8dff73e322a9612fae611
Andy Mallett <104897409+NotAndyforsure@users.noreply.github.com>
2022-10-25 07:53:42 +0100

    responsiveness update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let howManyColors=0
 
 const menuIcon=document.querySelector("#menuicon")
 const menu=document.querySelector("#menu")
-const main« document.querySelector("main")
+const main=document.querySelector("main")
 
 
 menu.classList.toggle("menu-on")
@@ -15,7 +15,7 @@ menuIcon.addEventListener("click",function(){
    
     
     menu.classList.toggle("menu-on")
-   menu.ClassList.toggle("mainwmenu")
+   main.ClassList.toggle("mainwmenu")
 
     
 })
